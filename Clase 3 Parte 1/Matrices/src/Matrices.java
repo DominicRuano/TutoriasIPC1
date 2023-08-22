b58f2bb5d4764f7b7ccc8d41c5c16eeeb6de7045
javieer24 <javiermonjesnuevo@gmail.com>
2023-08-22 17:06:58 -0600

    Clase 3 parte 1

---
+++ Clase 3 Parte 1/Matrices/src/Matrices.java
@@ -0,0 +1,17 @@
+public class Matrices{
+    public static void main(String args[]){
+
+        int numeros [][] = new int [2][2];
+
+        numeros [0][0] = 5;
+        numeros [0][1] = 2;
+        numeros [1][0] = 2;
+        numeros [1][1] = 5;
+
+
+        System.out.print("[" + numeros [0][0] + "]");
+        System.out.println("[" + numeros [0][1] + "]");
+        System.out.print("[" + numeros [1][0] + "]");
+        System.out.print("[" + numeros [1][1] + "]");
+     }
+}
